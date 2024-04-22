4b2adab899eeda1e546f04e68ba3f57be14ce66a
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2024-04-22 17:46:09 +0100

    added first and last names to users

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,6 @@ class Dog < ApplicationRecord
   validates :name, presence: true
   validates :breed, presence: true
   validates :description, presence: true
-  validates :available, presence: true
   validates :location, presence: true
   validates :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_16_181329) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_22_164347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.text "location"
+    t.float "price"
+    t.boolean "available"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_16_181329) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
